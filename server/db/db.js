48d783d385f3f998ad78f1f20f93a3f2747eaab7
marzouk2002 <marzoukyoussouf2002@gmail.com>
2022-04-03 23:42:43 +0000

    merge master

--- server/db/db.js
+++ server/db/db.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 require("dotenv").config();
-=======
-require('dotenv').config();
->>>>>>> master
 const Sequelize = require("sequelize");
 
 const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
